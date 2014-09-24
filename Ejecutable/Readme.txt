560506fef11152973ab4aaadb1c05d5b270f5235
Vladimir <viperezf@upbc.edu.mx>
2014-09-24 15:32:54 -0700

    Actividad 3: Revisada

--- Ejecutable/Readme.txt
+++ Ejecutable/Readme.txt
@@ -2,3 +2,4 @@ Listo, maestro. - Adrian Huitron
 
 Actividad 1: 10 Bien.
 Actividad 2: 9 a destiempo.
+Actividad 3: 10 Muy bien.
--- Source/Game.cpp
+++ Source/Game.cpp
@@ -16,20 +16,14 @@ void CGame::Finalize(){
 
 void CGame::Iniciando(){
 	if (SDL_Init(SDL_INIT_VIDEO)){
-
 		printf("Error %s ", SDL_GetError());
-
 		exit(EXIT_FAILURE);
-
 	}
 
 	screen = SDL_SetVideoMode(640, 480, 24, SDL_SWSURFACE);
 	if (screen == NULL){
-
 		printf("Error %s ", SDL_GetError());
-
 		exit(EXIT_FAILURE);
-
 	}
 
 	SDL_WM_SetCaption("Mi primer Juego", NULL);
